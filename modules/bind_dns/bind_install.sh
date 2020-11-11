57af7563268d8d536c53f37555e23b93267bb493
Gin <ginc11232@gmail.com>
2020-11-11 03:44:37 -0500

    Updated bind config file to public dns ref.

--- modules/bind_dns/bind_install.sh
+++ modules/bind_dns/bind_install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install -y bind9 bind9utils bind9-doc 
 cd /etc/bind 
 sudo rm named.conf.options 
-sudo wget https://raw.githubusercontent.com/GinSiuCheng/Temp/main/named.conf.options
+sudo wget https://raw.githubusercontent.com/GinSiuCheng/private-aks-deployment/master/modules/bind_dns/named.conf.options
 sudo named-checkconf
 sudo systemctl restart bind9
 sudo ufw allow bind9
